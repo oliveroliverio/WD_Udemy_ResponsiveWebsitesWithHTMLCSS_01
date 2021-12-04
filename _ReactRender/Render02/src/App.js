7cb17ddffd3bfa8e9d9e37585bae8601730147c8
Oliver Oliverio <oliveroliverio@gmail.com>
2021-12-04 13:45:30 -0800

    20211204_13:45

--- _ReactRender/Render02/src/App.js
+++ _ReactRender/Render02/src/App.js
@@ -1,10 +1,13 @@
-// import ExpenseItem from './components/ExpenseItem'
+import Item from './Components/Item'
+
+
+
+
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-
-
+      <Item></Item>
     </div>
   );
 }
